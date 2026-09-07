9db4593106dfc34fbc719ef8b064ea8f0d6bea27
JuanJF09 <ruan04342@gmail.com>
2026-09-06 21:43:57 -0300

    inicio

---
+++ parte_1_dart/bin/parte_1_dart.dart
@@ -0,0 +1,5 @@
+import 'package:parte_1_dart/parte_1_dart.dart' as parte_1_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${parte_1_dart.calculate()}!');
+}
---
+++ parte_1_dart/test/parte_1_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:parte_1_dart/parte_1_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
