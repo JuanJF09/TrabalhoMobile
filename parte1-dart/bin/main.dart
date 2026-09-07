b36c3c59069b18c6cfef8a6c7792c49fa34f4195
Kiry <caiokirydev15@gmail.com>
2026-09-07 11:20:28 -0300

    Update main.dart

--- parte1-dart/bin/main.dart
+++ parte1-dart/bin/main.dart
@@ -1,5 +1,30 @@
-import 'package:parte_1_dart/parte_1_dart.dart' as parte_1_dart;
+import 'models/MidiaFisica.dart';
+import 'models/MidiaOnline.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${parte_1_dart.calculate()}!');
+
+  final jogoFisico = MidiaFisica(
+    id: 'J02',
+    nome: 'Elden Ring',
+    genero: 'RPG de Acao',
+    preco: 249.90,
+    espacoArmazenamento: 60,
+    anoLancamento: 2022,
+    valorFrete: 25.00,
+    tipoEmbalagem: 'Steelbook Edicao de Colecionador',
+  );
+
+  final jogoOnline = MidiaOnline(
+    id: 'J03',
+    nome: 'Elden Ring',
+    genero: 'RPG de Acao',
+    preco: 229.90,
+    espacoArmazenamento: 60,
+    anoLancamento: 2022,
+    plataforma: 'Steam / PC',
+    requerConexaoConstante: true,
+  );
+
+  print(jogoFisico.ficha());
+  print(jogoOnline.ficha());
 }
