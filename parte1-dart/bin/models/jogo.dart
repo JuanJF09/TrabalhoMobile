421c16b2d7170f55c5124313ea929fa5ada0592d
JuanJF09 <ruan04342@gmail.com>
2026-09-06 22:56:27 -0300

    jogo.dart

---
+++ parte1-dart/bin/models/jogo.dart
@@ -0,0 +1,21 @@
+class Jogo {
+  String id;
+  String nome;
+  String genero;
+  double preco;
+  int espacoArmazenamento;
+  int anoLancamento;
+  bool disponivel;
+  DateTime dataCadastro;
+
+  Jogo({
+    required this.id,
+    required this.nome,
+    required this.genero,
+    required this.preco,
+    required this.espacoArmazenamento,
+    required this.anoLancamento,
+    this.disponivel = true,
+    DateTime? dataCadastro,
+  }) : dataCadastro = dataCadastro ?? DateTime.now();
+}
