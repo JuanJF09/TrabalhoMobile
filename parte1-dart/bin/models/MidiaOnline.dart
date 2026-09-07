a4543d70b41cbb8dc9ef91fdd09399e9babcc9c8
Kiry <caiokirydev15@gmail.com>
2026-09-07 11:20:07 -0300

    Add files via upload

---
+++ parte1-dart/bin/models/MidiaOnline.dart
@@ -0,0 +1,28 @@
+import 'jogo.dart';
+
+class MidiaOnline extends Jogo {
+    String plataforma;
+    bool requerConexaoConstante;
+
+    MidiaOnline(
+    {
+    required super.id,
+    required super.nome,
+    required super.genero,
+    required super.preco,
+    required super.espacoArmazenamento,
+    required super.anoLancamento,
+    super.disponivel,
+    super.dataCadastro,
+    
+    required this.plataforma,
+    required this.requerConexaoConstante,
+    });
+
+
+    @override
+    String ficha() {
+    final conexao = requerConexaoConstante ? 'Sim' : 'Não';
+    return '${super.ficha()} | Plataforma: $plataforma | Conexão Constante: $conexao';
+  }
+}
