3d5a953c4984ab21210213b6345f4204df79cb97
Kiry <caiokirydev15@gmail.com>
2026-09-12 03:05:46 -0300

    Refactor MidiaFisica class and update import statement

--- parte1-dart/bin/models/MidiaFisica.dart
+++ parte1-dart/bin/models/MidiaFisica.dart
@@ -1,11 +1,10 @@
-import 'jogo.dart';
+import 'Jogo.dart';
 
 class MidiaFisica extends Jogo {
-    double valorFrete;
-    String tipoEmbalagem;
+  double valorFrete;
+  String tipoEmbalagem;
 
-    MidiaFisica(
-    {
+  MidiaFisica({
     required super.id,
     required super.nome,
     required super.genero,
@@ -14,13 +13,13 @@ class MidiaFisica extends Jogo {
     required super.anoLancamento,
     super.disponivel,
     super.dataCadastro,
-    
+
     required this.valorFrete,
     required this.tipoEmbalagem,
-    });
+  });
 
-    @override
-    String ficha() {
-        return '${super.ficha()} | Frete: R\$ ${valorFrete.toStringAsFixed(2)} | Embalagem: $tipoEmbalagem';
-    }
+  @override
+  String ficha() {
+    return '${super.ficha()} | Frete: R\$ ${valorFrete.toStringAsFixed(2)} | Embalagem: $tipoEmbalagem';
+  }
 }
