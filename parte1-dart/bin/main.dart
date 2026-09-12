68a4d7ba4c2fb10d63ab8fd84dee97d4d9e317c2
Kiry <caiokirydev15@gmail.com>
2026-09-12 03:07:08 -0300

    Implement game library management and display features
    Added functionality to manage a game library and display game details.

--- parte1-dart/bin/main.dart
+++ parte1-dart/bin/main.dart
@@ -1,7 +1,17 @@
+import 'models/Jogo.dart';
 import 'models/MidiaFisica.dart';
 import 'models/MidiaOnline.dart';
+import 'models/Biblioteca.dart';
 
 void main(List<String> arguments) {
+  final jogoBase = Jogo(
+    id: 'J01',
+    nome: 'Jogo Base',
+    genero: 'padrao',
+    preco: 0.00,
+    espacoArmazenamento: 1,
+    anoLancamento: 0000,
+  );
 
   final jogoFisico = MidiaFisica(
     id: 'J02',
@@ -16,15 +26,39 @@ void main(List<String> arguments) {
 
   final jogoOnline = MidiaOnline(
     id: 'J03',
-    nome: 'Elden Ring',
+    nome: 'Hollow Knight',
     genero: 'RPG de Acao',
-    preco: 229.90,
-    espacoArmazenamento: 60,
-    anoLancamento: 2022,
+    preco: 49.90,
+    espacoArmazenamento: 10,
+    anoLancamento: 2017,
     plataforma: 'Steam / PC',
     requerConexaoConstante: true,
   );
 
-  print(jogoFisico.ficha());
-  print(jogoOnline.ficha());
+  print('=== Exercício 2: ficha() na classe base e nas subclasses  ===');
+  print(jogoBase.ficha()); // saída do método na classe base (Jogo)
+  print(jogoFisico.ficha()); // saída do método sobrescrito (MidiaFisica)
+  print(jogoOnline.ficha()); // saída do método sobrescrito (MidiaOnline)
+
+  print('');
+  final biblioteca = Biblioteca(nome: 'Acervo Pessoal');
+
+  print('\n=== Exercício 4: getter calculado antes de adicionar ===');
+  print('Quantidade de jogos: ${biblioteca.quantidadeJogos}');
+  print(
+    'Valor total do acervo: R\$ ${biblioteca.valorTotalAcervo.toStringAsFixed(2)}',
+  );
+
+  biblioteca.adicionar(jogoBase);
+  biblioteca.adicionar(jogoFisico);
+  biblioteca.adicionar(jogoOnline);
+
+  print('\n=== Exercício 4: getter calculado depois de adicionar ===');
+  print('Quantidade de jogos: ${biblioteca.quantidadeJogos}');
+  print(
+    'Valor total do acervo: R\$ ${biblioteca.valorTotalAcervo.toStringAsFixed(2)}',
+  );
+
+  print('\n=== Exercício 3: itens contidos na Biblioteca ===');
+  biblioteca.listarJogos();
 }
